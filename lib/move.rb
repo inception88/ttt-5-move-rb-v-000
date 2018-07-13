1a34180175d29a0f0ec3cc7b0ee66b479dfcbb80
Benjamin Jones <bljones36@gmail.com>
2018-07-13 22:12:28 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,9 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input = user_input.to_i
-  user_input = user_input - 1
+def input_to_index(index)
+  index = user_input.to_i
+  index = user_input - 1
 end
 
 def move(array,index,letter = "X")
